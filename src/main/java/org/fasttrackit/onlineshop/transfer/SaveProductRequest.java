6405475632896e85a3943ccf04bc30343d1dd74e
Edu <educreta5@gmail.com>
2020-02-26 20:39:34 +0200

    Added  integration tests for creating products

--- src/main/java/org/fasttrackit/onlineshop/transfer/SaveProductRequest.java
+++ src/main/java/org/fasttrackit/onlineshop/transfer/SaveProductRequest.java
@@ -1,41 +1,22 @@
-package org.fasttrackit.onlineshop.domain;
+package org.fasttrackit.onlineshop.transfer;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 
-@Entity
-public class Product {
+public class SaveProductRequest {
 
-
-    @Id
-    @GeneratedValue
     private long id;
     @NotNull
     private String name;
     @NotNull
     private String description;
-    private double price;
+    private Double price;
     @NotNull
-    private int quantity;
+    private Integer quantity;
     @NotNull
     private String imageUrl;
 
     public long getId() {
         return id;
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", price=" + price +
-                ", quantity=" + quantity +
-                ", imageUrl='" + imageUrl + '\'' +
-                '}';
     }
 
     public void setId(long id) {
@@ -58,20 +39,32 @@ public class Product {
         this.description = description;
     }
 
-    public double getPrice() {
+    public Double getPrice() {
         return price;
     }
 
-    public void setPrice(double price) {
+    public void setPrice(Double price) {
         this.price = price;
     }
 
-    public int getQuantity() {
+    public Integer getQuantity() {
         return quantity;
     }
 
-    public void setQuantity(int quantity) {
+    public void setQuantity(Integer quantity) {
         this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "SaveProductRequest{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                ", imageUrl='" + imageUrl + '\'' +
+                '}';
     }
 
     public String getImageUrl() {

08a8b2d0239b6193ce20a50ad83218d58aa0dd0c
Edu <educreta5@gmail.com>
2020-02-26 21:26:54 +0200

    added getProduct by Id (studied lambdas , optional and custom exceptions)

--- src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
+++ src/main/java/org/fasttrackit/onlineshop/service/ProductService.java
@@ -1,12 +1,15 @@
 package org.fasttrackit.onlineshop.service;
 
 import org.fasttrackit.onlineshop.domain.Product;
+import org.fasttrackit.onlineshop.exception.ResourceNotFoundException;
 import org.fasttrackit.onlineshop.persistance.ProductRepository;
 import org.fasttrackit.onlineshop.transfer.SaveProductRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -16,22 +19,44 @@ public class ProductService {
     private final ProductRepository productRepository;
 
     // Dependency Injection (from IoC container)
-@Autowired
+    @Autowired
     public ProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
 
     public Product createProduct(SaveProductRequest request) {
-    LOGGER.info("Creating product()", request);
+        LOGGER.info("Creating product()", request);
 
-    Product product = new Product();
-    product.setName(request.getName());
-    product.setDescription(request.getDescription());
-    product.setPrice(request.getPrice());
-    product.setQuantity(request.getQuantity());
-    product.setImageUrl(request.getImageUrl());
-    return productRepository.save(product);
+        Product product = new Product();
+        product.setName(request.getName());
+        product.setDescription(request.getDescription());
+        product.setPrice(request.getPrice());
+        product.setQuantity(request.getQuantity());
+        product.setImageUrl(request.getImageUrl());
+        return productRepository.save(product);
     }
 
+    public Product getProduct(long id) {
 
+        LOGGER.info("Retrieving product {}", id);
+
+        // optional usage explained
+
+       // productRepository.findById(id);
+
+        //Optional<Product> productOptional = productRepository.findById(id);
+
+//        if (productOptional.isPresent()) {
+
+  //          return productOptional.get();
+    //    } else {
+      //      throw new ResourceNotFoundException("Product" + id + "not found.");
+
+
+        //}
+
+        return productRepository.findById(id)
+                // lambda expressions
+                .orElseThrow(() -> new ResourceNotFoundException("Product" + id + "not found."));
+    }
 }
